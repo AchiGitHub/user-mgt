cbeabc43411ce97063ea7b5ead38abc5795a0735
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2022-12-27 09:51:29 +0530

    added missing auth files

--- lifestyle/src/main/java/com/lifestyleservice/lifestyle/controller/AuthController.java
+++ lifestyle/src/main/java/com/lifestyleservice/lifestyle/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
